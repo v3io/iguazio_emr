0317fa78abe5aac259225d19fdb834eb7e940d85
Evgeny Rachlenko <evgeny.rachlenko@gmail.com>
2018-02-08 15:52:14 +0200

    added new config to test run

--- bin/run_test_cluster.sh
+++ bin/run_test_cluster.sh
@@ -3,7 +3,7 @@ cd ./AWS/EMR/emr_install/emr-5.6.0/
 export PATH="$HOME/cli-ve/bin/:$PATH"
 if [ -f ./install_emr-5.6.0.sh ]; then 
 
-./install_emr-5.6.0.sh ./config/install_emr-5.6.0.cfg
+./install_emr-5.6.0.sh ./config/naipi_install_emr-5.6.0.cfg
 
 else
 	echo "you are in the wrong directory "
