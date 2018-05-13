1c177ea4e28dd8a1501f4abeb1961391a0fd4421
Evgeny Rachlenko <evgeny.rachlenko@gmail.com>
2018-05-14 00:19:27 +0300

    adding  config for log4j

--- AWS/EMR/s3_bucket/emr-5.6.0/download-5.6.0.sh
+++ AWS/EMR/s3_bucket/emr-5.6.0/download-5.6.0.sh
@@ -231,6 +231,15 @@ function presto_installation()
   sudo mv $v3io_properties  /etc/presto/conf/catalog/v3io.properties
 }
 
+function log_config()
+{
+	cat /etc/hadoop/conf/log4j.properties > /tmp/1
+	echo "log4j.logger.vendor.akka.event.slf_4j.Slf4jLogger=WARN" >> /tmp/1 
+	sudo mv /tmp/1 /etc/hadoop/conf/log4j.properties
+        sudo chown root:root  /etc/hadoop/conf/log4j.properties
+	sudo chmod 655 /etc/hadoop/conf/log4j.properties
+
+}
 
 function main()
 {
@@ -257,19 +266,13 @@ function main()
     sysctl_update
     change_ulimit
     presto_installation
- 
+    log_config
+
     # Copy post-installation artifacts and change permissions
     sudo chmod 755 /opt/igz/spark/lib/*.sh
     . /opt/igz/spark/lib/post_install_${IGZ_EMR_VERSION}.sh &
-
     logger -T "Iguazio installation done"
-    #add conf to hadoop logger
-    echo "log4j.logger.vendor.akka.event.slf_4j.Slf4jLogger=WARN" >> /etc/hadoop/conf/log4j.properties
-
- 
 }
 
 # Execute the script
 main $@
-
-
