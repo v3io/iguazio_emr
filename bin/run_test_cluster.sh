9c8f9c778b94178e787393da879267986bced708
Evgeny Rachlenko <evgenyr@iguazio.com>
2018-06-03 16:12:35 +0300

    added local aws path

--- bin/run_test_cluster.sh
+++ bin/run_test_cluster.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ./AWS/EMR/emr_install/emr-5.6.0/
-export PATH="$HOME/cli-ve/bin/:$PATH"
+export PATH="$PATH:$HOME/.local/bin/aws:$HOME/cli-ve/bin"
 if [ -f ./install_emr-5.6.0.sh ]; then 
 
 ./install_emr-5.6.0.sh ./config/naipi_install_emr-5.6.0.cfg
