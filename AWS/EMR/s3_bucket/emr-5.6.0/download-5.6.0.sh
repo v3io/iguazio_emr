acdc05add8d7ea87d728c531099584b5044d4a78
Vadim Kryvian <vadimk@iguazio.com>
2018-06-11 12:43:25 +0300

    fixed sym link to bigdata libs folder

--- AWS/EMR/s3_bucket/emr-5.6.0/download-5.6.0.sh
+++ AWS/EMR/s3_bucket/emr-5.6.0/download-5.6.0.sh
@@ -280,9 +280,10 @@ function main()
  
 
     # Copy post-installation artifacts and change permissions
+    sudo mkdir /home/iguazio/igz/bigdata/libs 
     sudo chmod 755 /opt/igz/spark/lib/*.sh
     . /opt/igz/spark/lib/post_install_${IGZ_EMR_VERSION}.sh &
-    sudo ln -s /home/iguazio/igz/bigdata/libs/* /home/iguazio/igz/bigdata/libs/
+    sudo ln -s /opt/igz/spark/lib/* /home/iguazio/igz/bigdata/libs/
     logger -T "Iguazio installation done"
 }
 
