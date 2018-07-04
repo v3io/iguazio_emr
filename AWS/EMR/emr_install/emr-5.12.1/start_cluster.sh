24797b30feb5172c95283790a9c59358678cdef3
sasile <sasi@iguaz.io>
2018-07-04 16:24:14 +0300

    fix missing cfg attributes for 5.12.1

--- AWS/EMR/emr_install/emr-5.12.1/start_cluster.sh
+++ AWS/EMR/emr_install/emr-5.12.1/start_cluster.sh
@@ -1,5 +1,5 @@
 IGZ_EMR_VERSION="5.12.1"
-./install_emr-$IGZ_EMR_VERSION.sh ./config/install_emr-$IGZ_EMR_VERSION.cfg
+./install_emr-$IGZ_EMR_VERSION.sh ./config/naipi_install_emr-$IGZ_EMR_VERSION.cfg
 ClusterId=`cat /tmp/ClusterId`
 EMRMASTER="/tmp/EMRMASTER"
 aws emr describe-cluster --cluster-id $ClusterId | python -c "import sys, json; print json.load(sys.stdin)['Cluster']['MasterPublicDnsName']"  > $EMRMASTER
